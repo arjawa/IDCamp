bb38a989c50c56ad3c966ed2466fe8e7f6c510f2
briexenv <briexenv@gmail.com>
2022-06-23 09:59:21 +0800

    Upload all files

---
+++ briexcube/js/main.js
@@ -0,0 +1,38 @@
+const navbarButton = document.querySelector(".navbar-toggler");
+const articleEven = document.querySelectorAll("#content > :nth-child(even)");
+const closeAlertButton = document.querySelector(".alert-close");
+const loadCounter = document.querySelector("#loadCounter");
+
+// Navbar handler
+navbarButton.addEventListener("click", () => {
+  document.querySelector(".navbar-collapse").classList.toggle("collapse");
+  navbarButton.classList.toggle("active");
+});
+
+// Article layouting
+articleEven.forEach(article => {
+  article.classList.add("row-reverse");
+});
+
+// Alert handler
+closeAlertButton.addEventListener("click", () => {
+  document.querySelector(".alert").style.display = "none";
+});
+
+// LocalStorage data handler
+window.onload = () => {
+  if (typeof(Storage) !== 'undefined') {
+  // Browser mendukung sessionStorage/localStorage.
+  let savedCount = Number(localStorage.getItem("loadCount"));
+    if (savedCount && savedCount >= 1 ) {
+      loadCounter.innerHTML = `<span>This page is loaded ${savedCount + 1} times on your browser.</span>`;
+      localStorage.loadCount = savedCount + 1;
+    } else {
+      loadCounter.innerHTML = "<span>This page is successfully loaded on your browser.</span>";
+      localStorage.loadCount = 1;
+    }
+  } else {
+    // Browser tidak mendukung sessionStorage/LocalStorage
+    alert("Please consider to use another browser.")
+  }
+}
